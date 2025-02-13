fe338a5d2f85271f858fc16d400d3f9907cd98aa
Alexis Sánchez <aasanchez@gmail.com>
2025-02-13 13:41:25 +0100

    Add gitattributes for text files and update build script

--- build.sh
+++ build.sh
@@ -6,34 +6,34 @@ ARCH=all
 
 rm -rf pkg/sys-info_*
 
-mkdir -p pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/usr/bin
-cp -r sys-info pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/usr/bin
-sudo chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/usr/bin/sys-info
+# mkdir -p pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/usr/bin
+# cp -r sys-info pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/usr/bin
+# sudo chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/usr/bin/sys-info
 
-# Debian package structure
-mkdir -p pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN
-cp pkg/scripts/control  pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/control
-sed -i "s/<VERSION>/"$GIT_TAG"-"$BUILD_DATE"/g" pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/control
-sed -i "s/<ARCH>/"$ARCH"/g" pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/control
+# # Debian package structure
+# mkdir -p pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN
+# cp pkg/scripts/control  pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/control
+# sed -i "s/<VERSION>/"$GIT_TAG"-"$BUILD_DATE"/g" pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/control
+# sed -i "s/<ARCH>/"$ARCH"/g" pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/control
 
-# Pre installation script
-cp pkg/scripts/preinst pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/preinst
-chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/preinst
+# # Pre installation script
+# cp pkg/scripts/preinst pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/preinst
+# chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/preinst
 
-# Post installation script
-cp pkg/scripts/postinst pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/postinst
-chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/postinst
+# # Post installation script
+# cp pkg/scripts/postinst pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/postinst
+# chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/postinst
 
-# Pre Removal script
-cp pkg/scripts/prerm pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/prerm
-chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/prerm
+# # Pre Removal script
+# cp pkg/scripts/prerm pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/prerm
+# chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/prerm
 
-# Post Removal script
-cp pkg/scripts/postrm pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/postrm
-chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/postrm
+# # Post Removal script
+# cp pkg/scripts/postrm pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/postrm
+# chmod 755 pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"/DEBIAN/postrm
 
-dpkg --build pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"
-cp pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH".deb sys-info.deb
+# dpkg --build pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH"
+# cp pkg/sys-info_"$GIT_TAG"-"$BUILD_DATE"_"$ARCH".deb sys-info.deb
 
-dpkg-deb --info sys-info.deb
-sudo dpkg -i sys-info.deb
+# dpkg-deb --info sys-info.deb
+# sudo dpkg -i sys-info.deb
