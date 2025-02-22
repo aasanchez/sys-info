90850a9504e04bd10a07c42455381f98859d0454
aasanchez <aasanchez@gmail.com>
2025-02-22 02:26:39 +0000

    Upgrading version to: 0.1.34 from 0.1.33

--- src/sys-info.c
+++ src/sys-info.c
@@ -14,7 +14,7 @@ int main() {
   printf(BOLD "This is a demo package\n" RESET);
 
   printf("\n" RED);
-  printf("Version: 0.1.33");
+  printf("Version: 0.1.34");
   printf(RESET "\n");
   printf("\n");
   // Print compilation date and time
