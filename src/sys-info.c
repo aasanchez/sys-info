799bca6b331a60cb847aeabb2d17328b695e2c69
aasanchez <aasanchez@gmail.com>
2025-03-02 02:33:21 +0000

    Upgrading version to: 1.2.42 from 1.1.42

--- src/sys-info.c
+++ src/sys-info.c
@@ -14,7 +14,7 @@ int main() {
   printf(BOLD "This is a demo package\n" RESET);
 
   printf("\n" RED);
-  printf("Version: 1.1.42");
+  printf("Version: 1.2.0");
   printf(RESET "\n");
   printf("\n");
   // Print compilation date and time
