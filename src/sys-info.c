0ae476bf700fe7c40110982e2dad94a98ecd1666
aasanchez <aasanchez@gmail.com>
2025-05-02 02:36:47 +0000

    Upgrading version to: 2.1.0 from 2.0.0

--- src/sys-info.c
+++ src/sys-info.c
@@ -14,7 +14,7 @@ int main() {
   printf(BOLD "This is a demo package\n" RESET);
 
   printf("\n" RED);
-  printf("Version: 2.0.0");
+  printf("Version: 2.1.0");
   printf(RESET "\n");
   printf("\n");
   // Print compilation date and time
