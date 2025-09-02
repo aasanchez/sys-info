32d67915f190ec8cb8c535bfc6de86ec42fc1548
aasanchez <aasanchez@gmail.com>
2025-09-02 02:36:27 +0000

    Upgrading version to: 4.0.0 from 3.1.5

--- src/sys-info.c
+++ src/sys-info.c
@@ -14,7 +14,7 @@ int main() {
   printf(BOLD "This is a demo package\n" RESET);
 
   printf("\n" RED);
-  printf("Version: 3.1.5");
+  printf("Version: 4.0.0");
   printf(RESET "\n");
   printf("\n");
   // Print compilation date and time
